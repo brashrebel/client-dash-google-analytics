2ab0b4ac7920eae198bc91f3b0b871070d3f87bf
Kyle <kyle@realbigmarketing.com>
2014-05-09 10:52:58 -0400

    testing
    Trying to get a connection. Login required apparently.

--- client-dash-google-analytics.php
+++ client-dash-google-analytics.php
@@ -6,5 +6,14 @@ Version: 0.1
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
+function cdga_test() {
+$url = 'https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A77904511&start-date=yesterday&end-date=today&metrics=ga%3Asessions&output=dataTable&samplingLevel=DEFAULT&key=AIzaSyBIjCeKlZIrTjKDyr16wVwFJY-27qmeWoE';
 
+	$response = wp_remote_get($url);
+	print '<pre>';
+	print_r($response);
+	print '</pre>';
+}
+
+add_shortcode('cdga', 'cdga_test');
 ?>
