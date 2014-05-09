07f0a785dbb13117e67c170921b9be34720b6443
Kyle <kyle@realbigmarketing.com>
2014-05-08 21:43:52 -0400

    Initial commit
    Getting started.

---
+++ client-dash-google-analytics.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Client Dash Google Analytics Add-on
+Description: Integrates Google Analytics reporting with Client Dash
+Version: 0.1
+Author: Kyle Maurer
+Author URI: http://realbigmarketing.com/staff/kyle
+*/
+
+?>
