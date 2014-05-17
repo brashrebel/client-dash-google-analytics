855479ecc369a46d6bfeb022a3de2c1c6ee69fcd
Kyle <kyle@realbigmarketing.com>
2014-05-17 15:17:43 -0400

    First launch
    Here we go.

--- client-dash-google-analytics.php
+++ client-dash-google-analytics.php
@@ -1,19 +1,14 @@
 <?php
 /*
-Plugin Name: Client Dash Google Analytics Add-on
-Description: Integrates Google Analytics reporting with Client Dash
+Plugin Name: Client Dash WP Help Add-on
+Description: Integrates content from WP Help with Client Dash
 Version: 0.1
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
-function cdga_test() {
-$url = 'https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A77904511&start-date=yesterday&end-date=today&metrics=ga%3Asessions&output=dataTable&samplingLevel=DEFAULT&key=AIzaSyBIjCeKlZIrTjKDyr16wVwFJY-27qmeWoE';
-
-	$response = wp_remote_get($url);
-	print '<pre>';
-	print_r($response);
-	print '</pre>';
+function cdwph_test() {
+echo 'test';
 }
 
-add_shortcode('cdga', 'cdga_test');
+add_shortcode('cdga', 'cdwph_test');
 ?>
