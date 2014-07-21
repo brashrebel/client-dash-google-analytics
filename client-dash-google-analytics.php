dc6ec5c1588e380e7bd80ce9e495537b136e57f8
brashrebel <kyle@realbigmarketing.com>
2014-07-21 18:13:35 -0400

    Using the boilerplate
    Ditched all old content and replaced it with customized copy of the
    boilerplate code.

--- client-dash-google-analytics.php
+++ client-dash-google-analytics.php
@@ -6,14 +6,113 @@ Version: 0.1
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
-function cdga_test() {
-$url = 'https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A77904511&start-date=yesterday&end-date=today&metrics=ga%3Asessions&output=dataTable&samplingLevel=DEFAULT&key=AIzaSyBIjCeKlZIrTjKDyr16wVwFJY-27qmeWoE';
 
-	$response = wp_remote_get($url);
-	print '<pre>';
-	print_r($response);
-	print '</pre>';
+class CDGoogleAnalytics {
+
+	/*
+	* These variables you can change
+	*/
+	// Define the plugin name
+	private $plugin = 'Client Dash Google Analytics';
+	// Setup your prefix
+	private $pre = 'cdga';
+	// Set this to be name of your content block
+	private $block_name = 'Google Analytics';
+	// Set the tab slug and name
+	private $tab = 'analytics';
+	// Set this to the page you want your tab to appear on (account, help and reports exist in Client Dash)
+	private $page = 'reports';
+
+	/*
+	* Now let's setup our options
+	* You can change the strings to be more unique
+	* If you change the variable names, you'll need to update the
+	* references in the register_settings() and settings_display() functions
+	*/
+	// A checkbox option
+	//private $cb_option = '_checkbox';
+	// A text field option
+	//private $text_option = '_text';
+	// A URL/text field option
+	private $url_option = '_api_url';
+
+	/*
+	* This constructor function sets up what happens when the plugin
+	* is activated. It is where you'll place all your actions, filters
+	* and other setup components.
+	*/
+	public function __construct() {
+		add_action( 'admin_notices', array( $this, 'notices' ) );
+		add_action( 'plugins_loaded', array( $this, 'content_block' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'cd_settings_general_tab', array( $this, 'settings_display' ), 11 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_styles' ) );
+	}
+
+	public function content_block() {
+		cd_content_block( $this->block_name, $this->page, $this->tab, array( $this, 'tab_contents' ) );
+	}
+
+	public function register_styles() {
+		wp_register_style( $this->pre , plugin_dir_url(__FILE__).'style.css' );
+		$page = get_current_screen();
+		$tab = $_GET['tab'];
+
+		if ( $page->id != $this->page && $tab != $this->tab )
+			return;
+
+		wp_enqueue_style( $this->pre );
+	}
+
+	// Notices for if CD is not active (no need to change)
+	public function notices() {
+		if ( !is_plugin_active( 'client-dash/client-dash.php' ) ) { ?>
+		<div class="error">
+			<p><?php echo $this->plugin; ?> requires <a href="http://w.org/plugins/client-dash">Client Dash</a>.
+			Please install and activate <b>Client Dash</b> to continue using.</p>
+		</div>
+		<?php
+		}
+	}
+
+	// Register settings
+	public function register_settings() {
+		//register_setting( 'cd_options_general', $this->pre.$this->cb_option );
+		//register_setting( 'cd_options_general', $this->pre.$this->text_option, 'esc_html' );
+		register_setting( 'cd_options_general', $this->pre.$this->url_option, 'esc_url_raw' );
+	}
+
+	// Add settings to General tab
+	public function settings_display() {
+		//$checkbox_option_name = $this->pre.$this->cb_option;
+		//$checkbox_option = get_option( $checkbox_option_name );
+		//$text_option = $this->pre.$this->text_option;
+		$url_option = $this->pre.$this->url_option;
+		?>
+	<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><h3><?php echo $this->plugin; ?> settings</th>
+			</tr>
+			<tr valign="top">
+				<th scope="row">
+					<label for="<?php echo $url_option; ?>">API URL</label>
+				</th>
+				<td><input type="text" 
+					id="<?php echo $url_option; ?>" 
+					name="<?php echo $url_option; ?>" 
+					value="<?php echo get_option( $url_option ); ?>" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<?php }
+
+	// Insert the tab contents
+	public function tab_contents() {
+		// CHANGE THIS
+		echo 'This is where your tab content goes.';
+	}
 }
-
-add_shortcode('cdga', 'cdga_test');
-?>
+// Instantiate the class
+$cdgoogleanalytics = new CDGoogleAnalytics;
